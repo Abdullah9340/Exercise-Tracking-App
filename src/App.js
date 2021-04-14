ec1a97757a37f30c81f4913c53c57d39aebad0ed
Abdullah9340 <abdullah.abdullah42@Outlook.com>
2021-04-14 15:46:45 -0400

    edited git ignore

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import {BrowserRouter as Router,Route} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Navbar from "./components/navbar";
+import ExerciseList from "./components/exerciseList";
+import EditExercise from "./components/editExercise";
+import CreateExercise from "./components/CreateExercise.component";
+import CreateUser from "./components/CreateUser.component";
+
 function App() {
   return (
     <Router>
       <Navbar />
       <br/>
-      <Route path = "/" exact component = {ExericseList}/>
+      <Route path = "/" exact component = {ExerciseList}/>
       <Route path = "/edit/:id"  component = {EditExercise}/>
       <Route path = "/create"  component = {CreateExercise}/>
       <Route path = "/user"  component = {CreateUser}/>
